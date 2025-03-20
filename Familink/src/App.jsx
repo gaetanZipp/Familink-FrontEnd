ce06265d962e1ea39842393cc916f1c620584bb3
gaetanPro <gaetanbagze995@gmail.com>
2025-03-20 16:41:06 +0100

    Ajout des autres pages

--- Familink/src/App.jsx
+++ Familink/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Navbar from './components/navbar/Navbar'
+import Section1 from './components/section1/Section1'
+import Section2 from './components/section2/Section2'
+import Section3 from './components/section3/Section3'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
+    <div>
       <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Footer />
+    </div>
   )
 }
 
