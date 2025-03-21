e281a06896b7e329546589f6ce64a70e4de94553
gaetanPro <gaetanbagze995@gmail.com>
2025-03-21 20:56:47 +0100

    Ajout du composant section1

--- Familink/src/components/section1/Section1.jsx
+++ Familink/src/components/section1/Section1.jsx
@@ -3,7 +3,16 @@ import './Section1.css'
 
 const Section1 = () => {
   return (
-    <div>Section1</div>
+    <div class="section1">
+      <div className="section1-left">
+      
+      </div>
+      <div className="section1-right">
+        <h1>Build Your Family Tree Online</h1>
+        <p>Family is important. Keep your family close with a family tree that will help you keep track of your relatives and ancestors.</p>
+        <button>Get Started</button>
+      </div>
+    </div>
   )
 }
 
