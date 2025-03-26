010bb831e74e439bde0e40ae057941a91dddfeb6
gaetanPro <gaetanbagze995@gmail.com>
2025-03-26 20:09:17 +0100

    Update-206-03-2025(2)

--- Familink/src/routes/AppRoutes.jsx
+++ Familink/src/routes/AppRoutes.jsx
@@ -5,7 +5,7 @@ import Main from '../components/MainPage';
 const router = createBrowserRouter([
    
     { path: "/", element: <Main /> },
-    { path: "BuildingPage", element: <BuildingPage /> }, 
+    { path: "/BuildingPage", element: <BuildingPage /> }, 
 
     // Ici tu ajoutes les routes que tu appelleras dans toutes tes pages
     // Donc pour faire simple tu centralises les routes ici
