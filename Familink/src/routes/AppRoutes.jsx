55d3efb312638145f6eacb866a86b0149b762ca8
Franck Judes <93270724+FranckJudes@users.noreply.github.com>
2025-03-26 17:31:36 +0100

    add routes files

---
+++ Familink/src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+
+const router = createBrowserRouter([
+   
+
+]);
+
+export default function AppRoutes() {
+    return <RouterProvider router={router} />;
+}
